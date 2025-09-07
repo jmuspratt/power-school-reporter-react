1dcea3e9a9385c95d58c6433c78d16d1295d3f5f
James Muspratt <james@jamesmuspratt.com>
2025-09-07 16:54:57 -0400

    Ensure hot reloading

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: 'power-school-reporter-react',
+  css: {
+    devSourcemap: true
+  },
+  server: {
+    hmr: true
+  }
 })
